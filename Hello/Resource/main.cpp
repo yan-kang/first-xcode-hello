fe5926d915cffc6e826a7ec8222b8a72403c35f1
yankang <2929481692@qq.com>
2020-10-17 15:11:28 +0800

    first commit

---
+++ Hello/Resource/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by kang on 2020/10/16.
+//
+#include "../Header/test.h"
+
+int main(int argc, const char * argv[]) {
+    //cout<<argv[1]<<endl;
+    const char * str1[3]={"你好","Who are you?","你在干嘛"};
+    const char * p1[3]={str1[0],str1[1],str1[2]};
+    for(int i=0;i<3;i++){
+        cout<<p1[i]<<" "<<*(p1+i)<<endl;
+    }
+    char str2[3][10]={"ddd","hhh","ss"};
+    char *p[3];
+    p[0]=str2[0];
+    p[1]=str2[1];
+    p[2]=str2[2];
+    for(int i=0;i<3;i++){
+        cout<<*(p+i)<<endl;
+    }
+    Test::stest();
+    Test tt;
+    tt.test(3, str1);
+    string str;
+    cout<<"Please input the string"<<endl;
+    cin>>str;
+    cout<<str<<endl;
+    return 0;
+}
+
